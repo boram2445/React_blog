1bb19281b92986ace7f4f41d8d1cbfb70861e3b8
Boram Kim <boram2445@gmail.com>
2022-06-13 22:25:01 +0900

    post 컴포넌트 data 처리

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,15 +1,15 @@
 import './posts.css';
 import Post from './post';
+import postData from '../data';
 
 function Posts(){
   return(
     <ul className="posts">
-      <Post img={'assets/post-img6.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']} title={'Lorem, ipsum dolor sit amet consectetur adipisicing elit'} content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."}/>
-      <Post img={'assets/post-img5.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']}/>
-      <Post img={'assets/post-img4.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']}/>
-      <Post img={'assets/post-img3.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']}/>
-      <Post img={'assets/post-img2.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']}/>
-      <Post img={'assets/post-img1.jpg'} author={'Chilli'} date={'2022.05.25'} category={['Life','style']}/>
+      {postData.map((value, index)=>{
+        return(
+          <Post key={index} img={value.img} author={value.author} date={value.date} category={value.category} title={value.title} content={value.content}/>
+        );
+      })}
     </ul>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const postData = [
+  {
+    img : 'assets/post-img6.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+  {
+    img : 'assets/post-img5.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+  {
+    img : 'assets/post-img4.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+  {
+    img : 'assets/post-img3.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+  {
+    img : 'assets/post-img2.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+  {
+    img : 'assets/post-img1.jpg',
+    author : 'Chilli',
+    date : '2022.05.25',
+    category : ['Life','style'],
+    title : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem estfacilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate solutacorrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventoreillum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilis dolor quas odio cum incidunt repudiandae vel. Lorem ipsumdolor sit amet consectetur, adipisicing elit. Inventore illum nostrum perferendis voluptas, voluptate soluta corrupti dolore quidem. Placeat, eaque! Exercitationem est facilisdolor quas odio cum incidunt repudiandae vel."
+  },
+];
+
+
+export default postData;
