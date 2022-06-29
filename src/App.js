4f70f0615bf1fe0ed44662994c41c70a536a2e9c
Boram Kim <boram2445@gmail.com>
2022-06-29 21:16:34 +0900

    Route 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
-import './css/reset.css';
-import './css/global.css';
-import PostPage from './pages/PostPage';
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import data from './asset/data.json';
-import { useState } from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import "./css/reset.css";
+import "./css/global.css";
+import PostPage from "./pages/PostPage";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import data from "./asset/data.json";
+import { useState } from "react";
 
 function App() {
-	let navigate = useNavigate();
-	return (<>
-		<Navbar navigate={navigate}/>
-		<Routes>
-			<Route path='/React_blog/' element={<Home data={data}/>}/>
-			<Route path='/React_blog/post/:id' element={<PostPage data={data} navigate={navigate}/>}/>
-			<Route path='*' element={<h1>잘못된 경로입니다</h1>}></Route>
-		</Routes>
-		<Footer/>
-	</>);
+  let navigate = useNavigate();
+  return (
+    <>
+      <Navbar navigate={navigate} />
+      <Routes>
+        <Route path="/React_blog/" element={<Home data={data} />} />
+        <Route
+          path="/React_blog/post/:id"
+          element={<PostPage data={data} navigate={navigate} />}
+        />
+        <Route path="*" element={<h1>잘못된 경로입니다</h1>}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
