980ba51ac90a6503e76ef772ef63d241c13272bc
Boram Kim <boram2445@gmail.com>
2022-06-29 21:17:48 +0900

    로그인 버튼 상태 기능 추가

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import './button.css';
 
-function Button({color='', icon, children}){
+function Button({color='', icon, handleLogin, children}){
   return(
-    <button className={`button ${color}`}>
+    <button className={`button ${color}`} onClick={()=>{handleLogin(children)}}>
       <img src={icon} alt="" />
       <span>{children}</span>
     </button>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,58 @@
-import './navbar.css';
-import Button from '../Button/Button';
-import logo from '../../assets/Logo.svg';
-import profileImg from '../../assets/profile.jpg';
-import modifyIcon from '../../assets/icon-modify-white.svg';
-import logoutIcon from '../../assets/icon-logout.svg';
+import "./navbar.css";
+import Button from "../Button/Button";
+import logo from "../../assets/Logo.svg";
+import profileImg from "../../assets/profile.jpg";
+import modifyIcon from "../../assets/icon-modify-white.svg";
+import logoutIcon from "../../assets/icon-logout.svg";
+import loginIcon from "../../assets/icon-login.svg";
+import registerIcon from "../../assets/icon-register.svg";
+import { useState } from "react";
 
-function Navbar({navigate}){
-  return(
+function Navbar({ navigate }) {
+  let [login, setLogin] = useState(false);
+	function handleLogin(title){
+		setLogin(!login);
+	}
+  return (
     <header>
-			<div className="max-width">
-				<h1>
-					<img src={logo} alt="My Blog" onClick={()=>{navigate('/React_blog/')}}/>
-				</h1>
-				<ul>
+      <div className="max-width">
+        <h1>
+          <img src={logo} alt="My Blog" onClick={() => {navigate("/React_blog/");}}/>
+        </h1>
+        <ul>
           {/* 로그인 */}
-					<li class="profile-img">
-						<img src={profileImg} alt="My Page" />
-					</li>
-					<li>
-            <Button color={'blue'} icon={modifyIcon}>Write</Button>
-					</li>
-					<li>
-            <Button color={'white'} icon={logoutIcon}>Logout</Button>
-					</li>
-				</ul>
-			</div>
-		</header>
+          {login === true 
+					? <>
+              <li class="profile-img">
+                <img src={profileImg} alt="My Page" />
+              </li>
+              <li>
+                <Button color={"blue"} icon={modifyIcon}>
+                  Write
+                </Button>
+              </li>
+              <li>
+                <Button color={"white"} icon={logoutIcon} handleLogin={handleLogin}>
+                  Logout
+                </Button>
+              </li>
+            </>
+          : <>
+              <li>
+                <Button color={"white"} icon={loginIcon} handleLogin={handleLogin}>
+                  Login
+                </Button>
+              </li>
+              <li>
+                <Button color={"white"} icon={registerIcon}>
+                  Register
+                </Button>
+              </li>
+            </>
+          }
+        </ul>
+      </div>
+    </header>
   );
 }
 
